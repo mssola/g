bc055613efcf2614939471aabf3b7117f436a549
Miquel Sabaté <mikisabate@gmail.com>
2015-01-06 17:52:15 +0100

    Happy new year :)

--- gcompletion.sh
+++ gcompletion.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013-2014 Miquel Sabaté Solà <mikisabate@gmail.com>
+# Copyright (C) 2013-2015 Miquel Sabaté Solà <mikisabate@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
