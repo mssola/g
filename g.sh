0f7ff3a89637a36100b2c349a6a0f22115a508dd
Miquel Sabaté Solà <msabate@suse.com>
2016-06-29 09:49:36 +0200

    Add copyright notice into the -v/--version flags
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- g.sh
+++ g.sh
@@ -112,7 +112,13 @@ g() {
     # Parse the command.
     case "$cmd" in
     -v | --version)
-        echo "g version $version"
+        cat <<HERE
+g version $version
+Copyright (C) 2013-2016 Miquel Sabaté Solà <mikisabate@gmail.com>
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+HERE
         ;;
     -h | --help)
         __g_help
